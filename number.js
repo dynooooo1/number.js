5eca21c9c6b9b389b267160d7a71eeaebc6931b2
dynooooo1 <dylandominno7@gmail.com>
2024-09-10 21:21:41 +0800

    Create number.js

---
+++ number.js
@@ -0,0 +1,31 @@
+// numbers.js
+
+// Step 1: Create variables to store your favorite numbers
+let firstFavNum = 7; // Replace 7 with your favorite number
+let secondFavNum = 3; // Replace 3 with your second favorite number
+
+// Step 2: Perform arithmetic operations
+
+// Addition
+let additionResult = firstFavNum + secondFavNum;
+console.log(Addition: ${firstFavNum} + ${secondFavNum} = ${additionResult});
+
+// Subtraction
+let subtractionResult = firstFavNum - secondFavNum;
+console.log(Subtraction: ${firstFavNum} - ${secondFavNum} = ${subtractionResult});
+
+// Multiplication
+let multiplicationResult = firstFavNum * secondFavNum;
+console.log(Multiplication: ${firstFavNum} * ${secondFavNum} = ${multiplicationResult});
+
+// Division
+let divisionResult = firstFavNum / secondFavNum;
+console.log(Division: ${firstFavNum} / ${secondFavNum} = ${divisionResult});
+
+// Modulus
+let modulusResult = firstFavNum % secondFavNum;
+console.log(Modulus: ${firstFavNum} % ${secondFavNum} = ${modulusResult});
+
+// Exponentiation (Power)
+let exponentiationResult = firstFavNum ** secondFavNum;
+console.log(Exponentiation: ${firstFavNum} ** ${secondFavNum} = ${exponentiationResult});
